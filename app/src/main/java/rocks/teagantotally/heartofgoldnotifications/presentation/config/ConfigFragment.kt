592ab02f73be91a4399e6726a9ca5bdc2e0ec79c
Teagan Glenn <that@teagantotally.rocks>
2019-03-08 17:09:45 -0700

    Make it a preference fragment

--- app/src/main/java/rocks/teagantotally/heartofgoldnotifications/presentation/config/ConfigFragment.kt
+++ app/src/main/java/rocks/teagantotally/heartofgoldnotifications/presentation/config/ConfigFragment.kt
@@ -1,24 +1,24 @@
 package rocks.teagantotally.heartofgoldnotifications.presentation.config
 
 import android.os.Bundle
+import android.preference.PreferenceFragment
 import android.support.v4.app.Fragment
+import android.support.v7.preference.PreferenceFragmentCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.coroutines.CoroutineScope
+import rocks.teagantotally.heartofgoldnotifications.R
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-class ConfigFragment : Fragment(), ConfigContract.View, CoroutineScope {
+class ConfigFragment : PreferenceFragmentCompat(), ConfigContract.View, CoroutineScope {
     override lateinit var presenter: ConfigContract.Presenter
     override lateinit var coroutineContext: CoroutineContext
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.connection_configuration, rootKey)
     }
 
     override fun setHost(host: String) {
